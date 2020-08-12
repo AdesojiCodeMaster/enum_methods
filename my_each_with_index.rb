a6a012093c24c78dbe0efaadad3011f16b4d3be9
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-12 10:28:38 -0300

    my_each_with_index sintax-related changes requested by linters

--- my_each_with_index.rb
+++ my_each_with_index.rb
@@ -1,7 +1,8 @@
 module Enumerable
   # print out individual element with corresponding index
-  def my_each_with_index 
+  def my_each_with_index
     return enum_for unless block_given? # to prevent block from throwing errors
+
     i = 0
     while i <= length - 1 # while loop
       yield(self[i], i)
