637df65a728083b1eb5c659d5ec457eef91eaa5f
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-12 22:25:06 +0100

    Remove errors in methods

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,32 +1,37 @@
+# frozen_string_literal: true
+
 module Enumerable
   # print out individual element
   def my_each
     return enum_for unless block_given? # to prevent block from throwing errors
-    
+
     i = 0
     while i <= length - 1 # while loop
       yield(self[i])
       i += 1
     end
   end
+
   # print out individual element with corresponding index
-  def my_each_with_index 
+  def my_each_with_index
     return enum_for unless block_given? # to prevent block from throwing errors
-    
+
     i = 0
     while i <= length - 1 # while loop
       yield(self[i], i)
       i += 1
     end
   end
+
   # print out a new array according to the select condition
   def my_select
     return enum_for unless block_given? # to prevent block from throwing errors
-    
+
     new_array = []
     my_each { |num| new_array << num if yield(num) } # iteration
     new_array
   end
+
   # give out true all elements met the conditions
   def my_all?(*input)
     result = true # to standadise
@@ -40,9 +45,9 @@ module Enumerable
     end
     result
   end
+
   # my_any? looks like opposite of my_all? return true if any of the elements obeys
   def my_any?(*input)
-    
     result = false
     if !input[0].nil?
       my_each { |n| result = true if input[0] == n }
@@ -54,11 +59,12 @@ module Enumerable
     end
     result
   end
+
   # give true or false in respect to the supplied condition
   def my_none?(input = nil)
     return true if count.zero? || (self[0].nil? && !include?(true))
     return false unless block_given? || !input.nil?
-    
+
     output = true
     if self.class == Array
       my_each do |n|
@@ -81,33 +87,36 @@ module Enumerable
     end
     output
   end
+
   # print out the number of elements
   def my_count(ele = nil)
     counter = 0
     if block_given? # to prevent block from throwing errors
       my_each { |n| counter += 1 if yield(n) }
     elsif ele
-        my_each { |n| counter += 1 if n == ele }
+      my_each { |n| counter += 1 if n == ele }
     else
       counter = length
     end
     counter
   end
+
   # print new array according to the condition
-  def my_map  
+  def my_map
     return enum_for unless block_given? # to prevent block from throwing errors
-    
+
     map_arr = []
     if self == Array
-        my_each { |n| map_arr << yield(n) }
+      my_each { |n| map_arr << yield(n) }
     else
-        my_each { |k, v| map_arr << yield(k, v) }
+      my_each { |k, v| map_arr << yield(k, v) }
     end
   end
+
   # to sum-up
   def my_inject(*input)
     return enum_for unless block_given? # to prevent block from throwing errors
-    
+
     argument = input + self
     return nil if argument.length.zero?
     return argument[0] if argument.length == 1
@@ -121,15 +130,16 @@ end
 def multiply_els(input)
   input.my_inject { |k, n| k * n }
 end
+
 # Modify our #my_map
-def my_map(proc = nil) 
+def my_map(proc = nil)
   return enum_for unless block_given? # to prevent block from throwing errors
 
   new_array = []
   if proc
     my_each { |n| new_array << pro.call(n) }
   else
-    self.each { |n| new_array << yield(n) }
+    each { |n| new_array << yield(n) }
   end
   new_array
 end
