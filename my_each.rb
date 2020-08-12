bd20c19b1cffa6e7061f9f81d50660e37f7ea949
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-12 14:04:00 -0300

    my_each remove extra end tag / fix indentation

--- my_each.rb
+++ my_each.rb
@@ -3,11 +3,10 @@ module Enumerable
   def my_each
     return enum_for unless block_given? # to prevent block from throwing errors
 
-      i = 0
-      while i <= length - 1 # while loop
-        yield(self[i])
-        i += 1
-      end
+    i = 0
+    while i <= length - 1 # while loop
+      yield(self[i])
+      i += 1
     end
   end
 end
