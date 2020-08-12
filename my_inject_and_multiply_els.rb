c4e795d77aa81cdb445cab744b4af8b18f71e64b
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-12 10:34:02 -0300

    my_inject_and_multiply_els sintax-related changes requested by linters

--- my_inject_and_multiply_els.rb
+++ my_inject_and_multiply_els.rb
@@ -1,14 +1,18 @@
 module Enumerables
-  def my_inject(*input) # to sum-up
-    return enum_for unless block_given? #to prevent block from throwing errors
+  def my_inject(*input)
+    # to sum-up
+    return enum_for unless block_given? # to prevent block from throwing errors
+
     argument = input + self
-    return nil if argument.length == 0
+    return nil if argument.empty?
     return argument[0] if argument.length == 1
+
     output = argument[0]
-    argument[1..argument.length - 1].my_each {|num| output = yield(output, num)}
-    return output
+    argument[1..argument.length - 1].my_each { |num| output = yield(output, num) }
+    output
   end
 end
-def multiply_els(input) # for multiplication
-  input.my_inject { |k, n| k * n}
+def multiply_els(input)
+  # for multiplication
+  input.my_inject { |k, n| k * n }
 end
