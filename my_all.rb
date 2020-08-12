899fb316bb656859d0b6986c1fffe93b02e299b7
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-12 10:19:58 -0300

    my_all make the sintax-related changes requested by linters

--- my_all.rb
+++ my_all.rb
@@ -1,7 +1,7 @@
 module Enumerable
-  # give out true all elements met the conditions
-  def my_all?(*input) 
-    result = true # to standadise
+  # return true when all elements meet the conditions
+  def my_all?(*input)
+    result = true # to standardise
     if !input[0].nil?
       my_each { |n| result = false unless input[0] == n }
     elsif !block_given?
@@ -9,7 +9,7 @@ module Enumerable
     elsif
       my_each { |n| result = false unless yield(n) }
     else
-      my_each{ |k, v| result = false unless yield(k, v) }
+      my_each { |k, v| result = false unless yield(k, v) }
     end
     result
   end
