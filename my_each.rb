56e83759030275f22c82dbf0c9f84a79881ecb8e
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-12 10:27:35 -0300

    my_each sintax-related changes requested by linters

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,13 @@
 module Enumerable
   # print out individual element
-  def my_each 
+  def my_each
     return enum_for unless block_given? # to prevent block from throwing errors
+
       i = 0
       while i <= length - 1 # while loop
         yield(self[i])
         i += 1
       end
+    end
   end
 end
