a2fb6f4350f3933ddf4fad2aa2c85b3c1152cb54
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-13 07:46:42 -0300

    Conflict solve for revert to previous commit (before optimizations)

--- my_any.rb
+++ my_any.rb
@@ -1,10 +1,14 @@
 module Enumerable
   # my_any? looks like opposite of my_all? return true if any of the elements obeys
   def my_any?(*input)
-    result = false # to standardise
-    my_each{
-      result = true if block_given? || yield(n)
-    }
+    result = false
+    if !input[0].nil?
+      my_each { |n| result = true if input[0] === n }
+    elsif !block_given?
+      my_each { |n| result = true if n }
+    else
+      my_each { |n| result = true if yield(n) } 
+    end
     result
   end
 end
