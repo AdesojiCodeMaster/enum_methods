c7848ac4730c4238566c1bfa79cfdd79f543f5f7
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-13 07:29:45 -0300

    my_none method optimize for reduce lines number and complexity

--- my_none.rb
+++ my_none.rb
@@ -1,29 +1,10 @@
 module Enumerable
   # give true or false in respect to the supplied condition
   def my_none?(input = nil)
-    return true if count.zero? || (self[0].nil? && !include?(true))
-    return false unless block_given? || !input.nil?
-
-    output = true
-    if self.class == Array
-      my_each do |n|
-        if block_given?
-          output = false if yield(n)
-        elsif input.class == Regexp
-          output = false if n.matche(input)
-        elsif input.class <= Numeric
-          output = false if n == input
-        elsif n.class <= input
-          output = false
-        end
-        break unless output
-      end
-    else
-      my_each do |k, v|
-        output = false if yield(k, v)
-        break unless output
-      end
-    end
-  output
+    result = true # to standardise
+    my_each{
+      result = false if block_given? && yield(n)
+    }
+    result
   end
 end
