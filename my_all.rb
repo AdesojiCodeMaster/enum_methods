7e58207917e648d2969563cb1cd9fe56225bb883
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-13 01:32:07 +0100

    Remove Trailing whitespace my_all.rb

--- my_all.rb
+++ my_all.rb
@@ -2,7 +2,7 @@ module Enumerable
   # return true when all elements meet the conditions
   def my_all?
     return enum_for unless block_given? # to prevent block from throwing errors
-    
+
     result = true # to standardise
     my_each { |n| result = false unless yield(n) }
     result
