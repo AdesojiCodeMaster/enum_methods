337df25a427a140fad75c949339089f0daf350ee
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-13 19:43:08 +0100

    Remove cyclomatic complexity errors in my_all.rb and my_any.rb

--- my_all.rb
+++ my_all.rb
@@ -2,18 +2,12 @@ module Enumerable
   # return true when all elements meet the conditions
   def my_all?(input = nil)
     result = true # to standardise
-    if !input.nil? && !input.is_a?(Class)
-      if input.is_a?(Regexp)
-        my_each { |n| return false unless !n[input].nil? || n[input] == 1 }
-      else
-        my_each { |n| return false unless n == input }
-      end
-    elsif input.is_a?(Class)
-      my_each { |n| return false unless n.is_a?(input) }
-    elsif input.nil? && block_given?
-      my_each { |n| return false unless yield n }
+    if input.is_a?(Regexp)
+      my_each { |n| return false unless !n[input].nil? || n[input] == 1 }
+    elsif !block_given?
+      my_each { |n| result = false unless n }
     else
-      my_each { |n| return false if n.nil? || n == false }
+      my_each { |n| result = false unless yield(n) }
     end
     result
   end
--- my_any.rb
+++ my_any.rb
@@ -2,18 +2,12 @@ module Enumerable
   # my_any? looks like opposite of my_all? return true if any of the elements obeys
   def my_any?(input = nil)
     result = false # to standardise
-    if length == 1
-      return false if self[0].nil? || self[0] == empty? || self[0] == false
-    elsif input.is_a?(Regexp)
+    if input.is_a?(Regexp)
       my_each { |n| return true if !n[input].nil? || n[input] == 1 }
-    elsif input.nil? && !block_given?
-      my_each { |n| return true if !n.nil? && n != false }
-    elsif !input.is_a?(Class) && !block_given?
-      my_each { |n| return true if n == input }
-    elsif input.is_a?(Class)
-      my_each { |n| return true if n.is_a?(input) }
-    elsif block_given?
-      my_each { |n| return true if yield n }
+    elsif !block_given?
+      my_each { |n| result = true if n }
+    else
+      my_each { |n| result = true if yield(n) }
     end
     result
   end
