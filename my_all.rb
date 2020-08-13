1ef2088d8b1bdc25175e79442cde2c977a2a4e23
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-13 08:01:20 -0300

    Manually revert my_all, my_any, and my_none files

--- my_all.rb
+++ my_all.rb
@@ -2,9 +2,14 @@ module Enumerable
   # return true when all elements meet the conditions
   def my_all?(*input)
     result = true # to standardise
-    my_each{
-      result = false unless block_given? && yield(n)
-    }
+    if !input[0].nil?
+      my_each { |n| result = false unless input[0] == n }
+    elsif !block_given?
+      my_each { |n| result = false unless n }
+    elsif my_each { |n| result = false unless yield(n) }
+    else
+      my_each { |k, v| result = false unless yield(k, v) }
+    end
     result
   end
 end
