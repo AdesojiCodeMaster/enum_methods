e486511df0d5904957da06b31220d82d51543ffc
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-13 16:57:03 +0100

    modify my_all.rb,my_any.rb and my_inject

--- my_all.rb
+++ my_all.rb
@@ -1,13 +1,19 @@
 module Enumerable
   # return true when all elements meet the conditions
-  def my_all?(*input)
+  def my_all?(input = nil)
     result = true # to standardise
-    if !input[0].nil?
-      my_each { |n| result = false unless input[0] === n }
-    elsif !block_given?
-      my_each { |n| result = false unless n }
+    if !input.nil? && !input.is_a?(Class)
+      if input.is_a?(Regexp)
+        my_each { |n| return false unless !n[input].nil? || n[input] == 1 }
+      else
+        my_each { |n| return false unless n == input }
+      end
+    elsif input.is_a?(Class)
+      my_each { |n| return false unless n.is_a?(input) }
+    elsif input.nil? && block_given?
+      my_each { |n| return false unless yield n }
     else
-      my_each { |n| result = false unless yield(n) }
+      my_each { |n| return false if n.nil? || n == false }
     end
     result
   end
