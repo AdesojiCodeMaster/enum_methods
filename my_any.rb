35b0ed15f85da99df0ea62564008a1cf59341efe
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-13 07:26:05 -0300

    my_any method optimize for reduce complexity

--- my_any.rb
+++ my_any.rb
@@ -1,15 +1,10 @@
 module Enumerable
   # my_any? looks like opposite of my_all? return true if any of the elements obeys
   def my_any?(*input)
-    result = false
-    if !input[0].nil?
-      my_each { |n| result = true if input[0] == n }
-    elsif !block_given?
-      my_each { |n| result = true if n }
-    elsif my_each { |n| result = true if yield(n) }
-    else
-      my_each { |k, v| result = true if yield(k, v) }
-    end
+    result = false # to standardise
+    my_each{
+      result = true if block_given? || yield(n)
+    }
     result
   end
 end
