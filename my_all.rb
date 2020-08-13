3abbbbc35bcce42386610d53d21eb6c94ca3454d
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-13 01:46:30 +0100

    Refactor code in m_any.rb and remove unnecessary statement form my_all.rb

--- my_all.rb
+++ my_all.rb
@@ -1,8 +1,6 @@
 module Enumerable
   # return true when all elements meet the conditions
   def my_all?
-    return enum_for unless block_given? # to prevent block from throwing errors
-
     result = true # to standardise
     my_each { |n| result = false unless yield(n) }
     result
--- my_any.rb
+++ my_any.rb
@@ -1,15 +1,8 @@
 module Enumerable
   # my_any? looks like opposite of my_all? return true if any of the elements obeys
-  def my_any?(*input)
+  def my_any?
     result = false
-    if !input[0].nil?
-      my_each { |n| result = true if input[0] == n }
-    elsif !block_given?
-      my_each { |n| result = true if n }
-    elsif my_each { |n| result = true if yield(n) }
-    else
-      my_each { |k, v| result = true if yield(k, v) }
-    end
+    my_each { |n| result = true if yield(n) }
     result
   end
 end
