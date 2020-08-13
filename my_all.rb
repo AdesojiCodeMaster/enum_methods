74d923afa171a4e7c257fda3cba5992e69e61706
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-13 03:37:09 +0100

    Correct  my_all.rb after reset to pass test examples

--- my_all.rb
+++ my_all.rb
@@ -3,12 +3,11 @@ module Enumerable
   def my_all?(*input)
     result = true # to standardise
     if !input[0].nil?
-      my_each { |n| result = false unless input[0] == n }
+      my_each { |n| result = false unless input[0] === n }
     elsif !block_given?
       my_each { |n| result = false unless n }
-    elsif my_each { |n| result = false unless yield(n) }
     else
-      my_each { |k, v| result = false unless yield(k, v) }
+      my_each { |n| result = false unless yield(n) }
     end
     result
   end
