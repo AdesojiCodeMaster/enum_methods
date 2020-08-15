c1c11bbe330fa3ee1abb26072c39d4e8cf0315e3
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-15 03:42:00 +0100

    disable disable Lint/ParenthesesAsGroupedExpression errors

--- my_map_modifier.rb
+++ my_map_modifier.rb
@@ -3,15 +3,15 @@ module Enumerable
   def my_map(proc = nil)
     # Modify our #my_map
     return enum_for unless block_given? # to prevent block from throwing errors
- 
-      new_array = []
-      if proc
-        my_each { |n| new_array << pro.call(n) }
-      else
-        each { |n| new_array << yield(n) }
-      end
-      new_array
+    
+    new_array = []
+    if proc
+      my_each { |n| new_array << pro.call(n) }
+    else
+      each { |n| new_array << yield(n) }
+    end
+    new_array
   end
 end 
 puts 'my_map method using Ruby doc test examples'
-p (1..4).my_map(nil) { |i| i * i } #=> [1, 4, 9, 16]
+(p) (1..4).my_map (nil) { |i| i * i } #=> [1, 4, 9, 16]
