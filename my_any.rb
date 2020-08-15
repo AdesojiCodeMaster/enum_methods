6d21a94ffa54d08b434b8a25fcd1d77dcb5fad18
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-15 01:30:05 +0100

    Retest my_any.rb with Ruby doc test examples

--- my_any.rb
+++ my_any.rb
@@ -1,3 +1,4 @@
+require_relative 'my_each.rb'
 module Enumerable
   # my_any? looks like opposite of my_all? return true if any of the elements obeys
   def my_any?(input = nil)
@@ -12,3 +13,11 @@ module Enumerable
     result
   end
 end
+
+puts 'my_any? method using Ruby doc test examples'
+p %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+p %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+p %w[ant bear cat].my_any?(/d/) #=> false
+p [nil, true, 99].my_any?(Integer) #=> true
+p [nil, true, 99].my_any? #=> true
+p [].my_any? #=> false
