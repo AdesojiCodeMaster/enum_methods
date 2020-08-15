c6e7dc62b44ca4b9b10b8037b17f95c2f62f641b
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-15 02:00:08 +0100

    Eliminate last PercentLiteralDelimiters errors

--- my_none.rb
+++ my_none.rb
@@ -7,7 +7,7 @@ module Enumerable
 end
 # please comment out test examples in my_any.rb before running my_none.rb to prevent interference!
 puts 'my_none? method using Ruby doc test examples'
-p %w{ant bear cat].my_none?(nil) { |word| word.length == 5 } #=> true
+p %w[ant bear cat].my_none?(nil) { |word| word.length == 5 } #=> true
 p %w[ant bear cat].my_none?(nil) { |word| word.length >= 4 } #=> false
 p %w[ant bear cat].my_none?(/d/) #=> true
 p [1, 3.14, 42].my_none?(Float) #=> false
