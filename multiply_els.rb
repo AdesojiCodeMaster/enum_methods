09023e340f347afaa5c696b508690de44ceabbc8
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-15 10:42:38 +0100

    Remove useless return error

--- multiply_els.rb
+++ multiply_els.rb
@@ -1,7 +1,8 @@
 require_relative 'my_inject.rb'
-# Please comment out the test examples in my_inject.rb before running this file to prevent interference
+# Please comment out the test examples in my_inject.rb file before running this file to prevent interference
 def multiply_els(input)
-  return input.my_inject { |sum, n| sum * n }
+  output = input.my_inject { |sum, n| sum * n }
+  output
 end
 puts 'multiply_els method using Ruby doc test examples'
 multiply_els([2, 4, 5]) #=> 40
