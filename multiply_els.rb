e6495807e3daf06182422fdcf1e645f572ddce9d
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-17 19:36:20 +0100

    Refactor the codes, rearrange the files into one and clean it up

--- multiply_els.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'my_inject.rb'
-# Please comment out the test examples in my_inject.rb file before running this file to prevent interference
-def multiply_els(input)
-  output = input.my_inject { |sum, n| sum * n }
-  output
-end
-puts 'multiply_els method using Ruby doc test examples'
-multiply_els([2, 4, 5]) #=> 40
