134a04fd68c180a2ab1f1a3a9ca0e575ae6ba48b
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-16 10:28:11 -0300

    RSpec for my_each_with_index on progress (ruby-doc test case proved to be wrong)

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -1,10 +1,21 @@
 require './enumerable_methods.rb'
-array = [1, 2, 3]
-puts array.my_each
+
+
+array_1 = [1, 2, 3]
 describe Enumerable do
   describe '#my_each' do
-    it 'array.each_cons(3) { |a| a } should return [1, 2, 3]' do
-      expect(array.my_each { |a| a }).to eql([1, 2, 3])
+    it '[1, 2, 3].each_cons(3) { |a| a } should return [1, 2, 3]' do
+      expect(array_1.my_each { |a| a }).to eql([1, 2, 3])
     end
   end
+
+  describe '#my_each_with_index' do
+    array_2 = ['cat', 'dog', 'wombat']
+    array_2_expected = {"cat"=>0, "dog"=>1, "wombat"=>2}
+    hash_new = Hash.new  
+    puts "TESTING RUBY-DOCS #{array_2.each_with_index { |item, index| hash_new[item] = index }}"
+    it '%w(cat dog wombat).my_each_with_index { |item, index| hash[item] = index } should return {"cat"=>0, "dog"=>1, "wombat"=>2}' do
+      expect(array_2.each_with_index { |item, index| hash_new[item] = index }).to eql(array_2_expected)
+    end
+  end  
 end
